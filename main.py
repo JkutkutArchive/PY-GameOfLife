5c496878f1433cb8eab27471eea4ae9882b01a7f
Jkutkut <jorgeregonzalez@gmail.com>
2020-04-20 13:37:59 +0200

    testing pygame

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import gamepy;
+import pygame as pyG;
+import numpy as np;
 
-print("hello world")
+pyG.init()
+
+width, height = 1000, 1000
+screen = pyG.display.set_mode((width, height))
+
+# bg = 25, 25, 25
+bg = 100, 100, 100
+
+screen.fill(bg)
+
+while True:
+    pass
