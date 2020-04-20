72e7577ba4b30efc73033a1528a09e82246f4a8d
Jkutkut <jorgeregonzalez@gmail.com>
2020-04-20 19:12:27 +0200

    finnish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame;
 import numpy as np;
+import math;
 import time
 
 def constant(f):
@@ -16,14 +17,16 @@ class color(object):
     @constant
     def GRID():
         return (128, 128, 128)
+    @constant
+    def WHITE():
+        return (255, 255, 255)
 
 
 pygame.init()
 pygame.display.set_caption("Game of Life")
 
 width, height = 1000, 1000
-width, height = 400, 400
-sizeX, sizeY = 20, 20
+sizeX, sizeY = 50, 50
 sizeWidthX = width / sizeX
 sizeWidthY = height / sizeY
 
@@ -34,13 +37,10 @@ screen = pygame.display.set_mode((width, height))
 # State of the cells: 0 = death, 1 = alive
 grid = np.matrix([[0 for j in range(sizeY)] for i in range(sizeX)])
 
-grid[1,3] = 1
-grid[2,3] = 1
-grid[3,3] = 1
-
-running = True
+gameRunning = True
+timeRunning = True
 # running = False
-while running:
+while gameRunning:
     screen.fill(COLOR.BG)
     newGrid = np.copy(grid)
     for x in range(sizeX):
@@ -51,39 +51,37 @@ while running:
                 ((x + 1) * sizeWidthX, (y + 1) * sizeWidthY),
                 (x * sizeWidthX, (y + 1) * sizeWidthY)
             ]
-            vecinitos = np.sum(grid[x - 1 : x + 2, y - 1 : y + 2]) - grid[x, y]
+            if timeRunning:
+                vecinitos = np.sum(grid[x - 1 : x + 2, y - 1 : y + 2]) - grid[x, y]
+                if(grid[x, y] == 0 and vecinitos == 3):
+                    newGrid[x, y] = 1 # new cell
+                elif(grid[x, y] == 1 and (vecinitos < 2 or vecinitos > 3)):
+                    newGrid[x, y] = 0 # over or under population
 
-            if(grid[x, y] == 1 and (vecinitos < 2 or vecinitos > 3)):
-                newGrid[x, y] = 0
-            elif(grid[x, y] == 0 and vecinitos == 3):
-                newGrid[x, y] = 1
-
+            # Draw
             if(grid[x, y] == 0):
                 pygame.draw.polygon(screen, COLOR.GRID, poli, 1)
             else:
-                pygame.draw.polygon(screen, COLOR.GRID, poli)
+                pygame.draw.polygon(screen, COLOR.WHITE, poli)
+
+
     grid = newGrid
     pygame.display.flip()
+    time.sleep(0.1)
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False;    
-    print("tick")
-    time.sleep(0.3)
-
-# x = 1
-# y = 1
-# index = 1
-# for i in range(0, sizeX):
-#     for j in range(0, sizeY):
-#         grid[i, j] = index
-#         index = index + 1
-
-# vecinito = grid[x - 1 : x + 2, y - 1 : y + 2]
-
-# print(len(grid))
-# print(grid)
-# print(vecinito)
+        if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0] == 1:
+            pos = pygame.mouse.get_pos()
+            x = math.floor(pos[0] / sizeWidthX)
+            y = math.floor(pos[1] / sizeWidthY)
+            # print((x, y))
+            grid[x, y] = (grid[x, y] - 1) % 2 
+        elif event.type == pygame.QUIT:
+            gameRunning = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == 32: # Space pressed
+                timeRunning = not timeRunning
+            # print(event.key)
 
 print("Thanks for playing, I hope you liked it.")
 pygame.quit()
