9ecff059dc1723effbf86e816e2b3547b2055753
Jkutkut <jorgeregonzalez@gmail.com>
2020-04-20 23:37:32 +0200

    now can install and unistall on linux

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 
+
+sudo cp gameOfLife.png /usr/share/icons/
+
 sudo cp gameOfLife.py /usr/bin/gameOfLife
+sudo chmod 755 /usr/bin/gameOfLife
 
 # create the .desktop file
 echo "[Desktop Entry]
@@ -8,8 +12,8 @@ Encoding=UTF-8
 Name=Game of Life
 Comment=Made by Jkutkut
 Exec=gameOfLife
-Icon=/home/jkutkut/github/PY-GameOfLife/gameOfLife.png
+Icon=/usr/share/icons/gameOfLife.png
 Terminal=false" >> gameOfLife.desktop
 
-sudo rm /usr/share/applications/gameOfLife.desktop
 sudo mv gameOfLife.desktop /usr/share/applications/
+echo "GameOfLife installed"
---
+++ unistall.sh
@@ -0,0 +1,4 @@
+sudo rm /usr/share/icons/gameOfLife.png
+sudo rm /usr/bin/gameOfLife
+sudo rm /usr/share/applications/gameOfLife.desktop
+echo "gameOfLife removed" 
