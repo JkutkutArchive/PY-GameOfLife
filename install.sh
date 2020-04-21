0794774da0b6d6f7ddb639c7cc8e269bfe326f7a
Jkutkut <jorgeregonzalez@gmail.com>
2020-04-21 10:54:34 +0200

    Stable version

--- install.sh
+++ install.sh
@@ -11,9 +11,13 @@ See more at https://github.com/Jkutkut
 Instalation will begin shortly.
 -Making sure that Python3 and the libraries needed are installed";
 
+echo "-Checking Python3:" &&
 sudo apt install python3 &&
+echo "-Checking pip3:" &&
 sudo apt install python3-pip &&
+echo "-Checking pygame:" &&
 sudo pip3 install pygame &&
+echo "-Checking numpy:" &&
 sudo pip3 install numpy ||
 (echo "
 ~~~~~~~~    ERROR AT INSTALLATION   ~~~~~~~~
@@ -26,12 +30,14 @@ echo "-------------------------------------------
 
 All things needed are correctly installed. Now installing the application.
 " &&
+echo "-Creating icon for the app" &&
 sudo cp gameOfLife.png /usr/share/icons/ && # move the icon to the correct dir
 
+echo "-Creating executable" &&
 sudo cp gameOfLife.py /usr/bin/gameOfLife && # move the python code
 sudo chmod 755 /usr/bin/gameOfLife && # make it able to be executed
 
-# create the .desktop file
+echo "-Creating Desktop Entry" &&
 echo "[Desktop Entry]
 Type=Application
 Encoding=UTF-8
@@ -39,10 +45,11 @@ Name=Game of Life
 Comment=Made by Jkutkut
 Exec=gameOfLife
 Icon=/usr/share/icons/gameOfLife.png
-Terminal=false" >> gameOfLife.desktop &&
+Terminal=false" >> gameOfLife.desktop && # create the .desktop file
 
 sudo mv gameOfLife.desktop /usr/share/applications/ &&
-echo "Installation ended.
+echo "
+Installation ended.
 Game of Life installed correctly" ||
 
 echo "
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,8 @@
-sudo rm /usr/share/icons/gameOfLife.png
-sudo rm /usr/bin/gameOfLife
-sudo rm /usr/share/applications/gameOfLife.desktop
-echo "gameOfLife removed" 
+sudo rm /usr/bin/gameOfLife &&
+sudo rm /usr/share/applications/gameOfLife.desktop &&
+sudo rm /usr/share/icons/gameOfLife.png &&
+echo "gameOfLife removed successfully" ||
+echo "
+~~~~~~~~    ERROR AT UNINSTALLATION   ~~~~~~~~
+    Not posible to complete the unistallation
+"
